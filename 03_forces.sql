14dbaeea9de351a2943e8a571027be1fe78dffcb
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-07-17 12:05:27 +1000

    added more summary results

--- 03_forces.sql
+++ 03_forces.sql
@@ -3,10 +3,15 @@
 
 -- average and total forces
 select test_name,
-       avg(total_force) as avg_force,
-       stddev_pop(total_force) as std_dev_force,
-       sum(total_force) as sum_force,
-       avg(total_force) * 350.0 as fred
+       avg(total_accel) as avg_total_accel,
+       avg(total_force) as avg_total_force,
+       max(total_accel) as max_total_accel,
+       max(total_force) as max_total_force,
+       sum(case when total_accel > 1.0 then 1 else 0 end) as over_1g_count,
+       sum(case when total_accel > 2.0 then 1 else 0 end) as over_2g_count,
+       sum(case when total_accel > 3.0 then 1 else 0 end) as over_3g_count,
+       sum(case when total_accel > 4.0 then 1 else 0 end) as over_4g_count,
+       sum(case when total_accel > 5.0 then 1 else 0 end) as over_5g_count
 from testing.bike_forces
 where test_name <> 'bike'
 group by test_name
